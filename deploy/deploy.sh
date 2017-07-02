90464354217640c6f84942409f20be0314e3ae6b
T <tjoshum@outlook.com>
2017-07-02 18:33:15 +0100

    Correct the IP address we use when running in linux.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,7 +16,8 @@ if [ $? -ne "0" ]; then
 fi
 
 if [ -z $DOCKER_ADDRESS ]; then
-  export DOCKER_ADDRESS="localhost"
+  # Assume we're running on linux, rather than a docker machine.
+  export DOCKER_ADDRESS=$(ifconfig docker0 | sed -n -e 's/.*inet addr:\([0-9]*\.*[0-9]*\.[0-9]\.[0-9]\).*/\1/p')
 fi
 
 docker-compose up -d --build database gamed
