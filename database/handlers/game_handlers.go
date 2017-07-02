6f95f84a9ab9112e9661b9b3722de4b1337d6d87
T <tjoshum@outlook.com>
2017-07-02 16:31:17 +0100

    Add get calls for games and expand unit tests.

--- database/handlers/game_handlers.go
+++ database/handlers/game_handlers.go
@@ -2,9 +2,8 @@ package handlers
 
 import (
 	"database/sql"
-	"log"
-
 	"fmt"
+	"log"
 
 	"github.com/tjoshum/acca-tracker/database/constants"
 	"github.com/tjoshum/acca-tracker/database/proto"
@@ -50,9 +49,69 @@ func (s *DatabaseHandler) AddGame(ctx context.Context, req *database.AddGameRequ
 	log.Println("Attempting SQL '" + str + "'...")
 	_, err = db.Exec(str)
 	if err != nil {
-		log.Println("Error: Example entry", err)
+		log.Println("Error: Table entry", err)
 		return err
 	}
+
+	// TODO db.LastInsertId
+
 	log.Println("Successfully added.")
+	return nil
+}
+
+func (s *DatabaseHandler) AddBet(ctx context.Context, req *database.AddBetRequest, rsp *database.AddBetResponse) error {
+	return nil
+}
+
+func (s *DatabaseHandler) AddUser(ctx context.Context, req *database.AddUserRequest, rsp *database.AddUserResponse) error {
+	return nil
+}
+
+func stringToTeamCode(str string) database.TeamCode {
+	return database.TeamCode(database.TeamCode_value[str])
+}
+
+func (s *DatabaseHandler) GetWeekGames(ctx context.Context, req *database.GetWeekGamesRequest, rsp *database.GetWeekGamesResponse) error {
+	db, err := GetDatabase()
+	if err != nil {
+		log.Println("Error: GetDatabase", err)
+		return nil
+	}
+	defer db.Close()
+
+	str := fmt.Sprintf(
+		"SELECT * FROM %s WHERE week = %d",
+		constants.GameTableName,
+		req.Week,
+	)
+	rows, err := db.Query(str)
+	if err != nil {
+		log.Println("Error: Table entry", err)
+		return err
+	}
+
+	for rows.Next() {
+		var (
+			gameId   int32
+			week     int32
+			homeTeam string
+			awayTeam string
+		)
+		if err := rows.Scan(&gameId, &week, &homeTeam, &awayTeam); err != nil {
+			log.Fatal(err)
+		}
+		println("Game", gameId, "week", week, "home team", homeTeam, "away team", awayTeam)
+
+		rsp.Games = append(rsp.Games, &database.GetWeekGamesResponse_Game{
+			gameId,
+			stringToTeamCode(homeTeam),
+			stringToTeamCode(awayTeam),
+		})
+	}
+
+	return nil
+}
+
+func (s *DatabaseHandler) GetGameBets(ctx context.Context, req *database.GetGameBetsRequest, rsp *database.GetGameBetsResponse) error {
 	return nil
 }
