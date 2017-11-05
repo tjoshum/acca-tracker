ef8251a5c94e8eaeb692152e9e2a3dc18926dafb
T <tjoshum@outlook.com>
2017-11-05 15:14:59 +0000

    Initial break apart of generating the bets table, and using it for formatting

---
+++ lib/table.go
@@ -0,0 +1,108 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	database "github.com/tjoshum/acca-tracker/database/proto"
+	"github.com/tjoshum/acca-tracker/lib/names"
+)
+
+// TODO Change to something more useful
+type DisplayBets struct {
+	DisplayString string
+	Class         string
+}
+
+// Type safety, to stop me from getting mixed up.
+type Username string
+type BetsOnAGame map[Username]DisplayBets
+type MyTable map[database.Game]BetsOnAGame // games -> {users -> bets}
+
+type UserMap map[int32]Username
+
+func getUserMapping(cl database.DatabaseServiceClient, ctx context.Context) UserMap {
+	rsp, err := cl.GetUserList(ctx, &database.GetUserListRequest{})
+	if err != nil {
+		log.Fatal("Error getting user list", err)
+	}
+	ret := make(UserMap)
+	for _, user := range rsp.GetUsers() {
+		ret[user.GetUserId()] = Username(user.GetUsername())
+	}
+	return ret
+}
+
+func getGames(cl database.DatabaseServiceClient, ctx context.Context, weekNum int32) []*database.Game {
+	rsp, err := cl.GetWeekGames(ctx, &database.GetWeekGamesRequest{
+		Week: weekNum,
+	})
+	if err != nil {
+		log.Fatal("Error getting games for week", weekNum, err)
+	}
+	fmt.Println("DEBUG webd getGames: size", len(rsp.GetGames()))
+	return rsp.GetGames()
+}
+
+func getBetsOnGame(cl database.DatabaseServiceClient, ctx context.Context, gameId int32) []*database.GetBetsOnGameResponse_Bet {
+	rsp, err := cl.GetBetsOnGame(ctx, &database.GetBetsOnGameRequest{
+		GameId: gameId,
+	})
+	if err != nil {
+		log.Fatal("Error getting bets for game", gameId, err)
+	}
+	return rsp.GetBets()
+}
+
+func getClassString(game database.Game, bet *database.GetBetsOnGameResponse_Bet) string {
+	if game.GetActive() {
+		if bet.GetBetOn() == game.GetHomeTeam() {
+			if (game.HomeScore + bet.Spread) > game.AwayScore {
+				return "winning"
+			} else {
+				return "losing"
+			}
+		} else { // Bet on the away team
+			if (game.AwayScore + bet.Spread) > game.HomeScore {
+				return "winning"
+			} else {
+				return "losing"
+			}
+		}
+	} else {
+		return "notstarted"
+	}
+}
+
+func CreateTable(week int32) MyTable {
+	// TODO Share between requests.
+	cl := database.NewDatabaseServiceClient(names.DatabaseSvc, client.DefaultClient)
+	ctx := metadata.NewContext(context.Background(), map[string]string{
+		"X-User-Id": "noone1235",
+		"X-From-Id": "some-client",
+	})
+
+	userMap := getUserMapping(cl, ctx)
+	games := getGames(cl, ctx, week)
+	fmt.Println("DEBUG webd Game size", len(games))
+	display_table := make(MyTable)
+	for _, a_game := range games {
+		game := *a_game
+		if display_table[game] == nil {
+			display_table[game] = make(BetsOnAGame)
+		}
+		fmt.Println("DEBUG webd Going round game loop", game)
+		bets_on_this_game := getBetsOnGame(cl, ctx, a_game.GameId)
+		for _, bet := range bets_on_this_game {
+			user_str := userMap[bet.GetUserId()]
+			fmt.Println("DEBUG webd Pushing ", game, "user:", user_str, "bet:", bet.GetBetOn(), bet.GetSpread())
+			displayString := fmt.Sprintf("%s (%d)", bet.BetOn, bet.Spread)
+			display_table[game][user_str] = DisplayBets{displayString, getClassString(game, bet)}
+		}
+	}
+
+	return display_table
+}
--- webd/main.go
+++ webd/main.go
@@ -1,115 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"html/template"
 	"net/http"
 	"sort"
 	"strconv"
 
-	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
-	"github.com/micro/go-micro/metadata"
-	"github.com/prometheus/common/log"
 	"github.com/tjoshum/acca-tracker/database/proto"
-	"github.com/tjoshum/acca-tracker/lib/names"
+	"github.com/tjoshum/acca-tracker/lib"
 )
-
-type displayBets struct {
-	DisplayString string
-	Class         string
-}
-
-// Type safety, to stop me from getting mixed up.
-type Username string
-type BetsOnAGame map[Username]displayBets
-type MyTable map[database.Game]BetsOnAGame // games -> {users -> bets}
-
-type UserMap map[int32]Username
-
-func GetUserMapping(cl database.DatabaseServiceClient, ctx context.Context) UserMap {
-	rsp, err := cl.GetUserList(ctx, &database.GetUserListRequest{})
-	if err != nil {
-		log.Fatal("Error getting user list", err)
-	}
-	ret := make(UserMap)
-	for _, user := range rsp.GetUsers() {
-		ret[user.GetUserId()] = Username(user.GetUsername())
-	}
-	return ret
-}
-
-func GetGames(cl database.DatabaseServiceClient, ctx context.Context, weekNum int32) []*database.Game {
-	rsp, err := cl.GetWeekGames(ctx, &database.GetWeekGamesRequest{
-		Week: weekNum,
-	})
-	if err != nil {
-		log.Fatal("Error getting games for week", weekNum, err)
-	}
-	fmt.Println("DEBUG webd GetGames: size", len(rsp.GetGames()))
-	return rsp.GetGames()
-}
-
-func GetBetsOnGame(cl database.DatabaseServiceClient, ctx context.Context, gameId int32) []*database.GetBetsOnGameResponse_Bet {
-	rsp, err := cl.GetBetsOnGame(ctx, &database.GetBetsOnGameRequest{
-		GameId: gameId,
-	})
-	if err != nil {
-		log.Fatal("Error getting bets for game", gameId, err)
-	}
-	return rsp.GetBets()
-}
-
-func GetClassString(game database.Game, bet *database.GetBetsOnGameResponse_Bet) string {
-	if game.GetActive() {
-		if bet.GetBetOn() == game.GetHomeTeam() {
-			if (game.HomeScore + bet.Spread) > game.AwayScore {
-				return "winning"
-			} else {
-				return "losing"
-			}
-		} else { // Bet on the away team
-			if (game.AwayScore + bet.Spread) > game.HomeScore {
-				return "winning"
-			} else {
-				return "losing"
-			}
-		}
-	} else {
-		return "notstarted"
-	}
-}
-
-func createTable(week int32) MyTable {
-	// TODO Share between requests.
-	cl := database.NewDatabaseServiceClient(names.DatabaseSvc, client.DefaultClient)
-	ctx := metadata.NewContext(context.Background(), map[string]string{
-		"X-User-Id": "noone1235",
-		"X-From-Id": "some-client",
-	})
-
-	userMap := GetUserMapping(cl, ctx)
-	games := GetGames(cl, ctx, week)
-	fmt.Println("DEBUG webd Game size", len(games))
-	display_table := make(MyTable)
-	for _, a_game := range games {
-		game := *a_game
-		if display_table[game] == nil {
-			display_table[game] = make(BetsOnAGame)
-		}
-		fmt.Println("DEBUG webd Going round game loop", game)
-		bets_on_this_game := GetBetsOnGame(cl, ctx, a_game.GameId)
-		for _, bet := range bets_on_this_game {
-			user_str := userMap[bet.GetUserId()]
-			fmt.Println("DEBUG webd Pushing ", game, "user:", user_str, "bet:", bet.GetBetOn(), bet.GetSpread())
-			displayString := fmt.Sprintf("%s (%d)", bet.BetOn, bet.Spread)
-			display_table[game][user_str] = displayBets{displayString, GetClassString(game, bet)}
-		}
-	}
-
-	return display_table
-}
 
 // Data to be passed in to the header html template
 type HeaderData struct {
@@ -121,7 +22,7 @@ type HeaderData struct {
 type RowData struct {
 	Game        database.Game
 	RowColour   string
-	Predictions []displayBets
+	Predictions []utils.DisplayBets
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, d interface{}) {
@@ -170,7 +71,7 @@ func weekViewHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Failed to find week"))
 		return
 	}
-	localTable := createTable(int32(week))
+	localTable := utils.CreateTable(int32(week))
 
 	d := &HeaderData{
 		Title: "NFL Betting Results",
@@ -179,7 +80,7 @@ func weekViewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "head", d)
 
 	var users []string // De-duplicated list of users who have placed bets this week.
-	workspace := make(map[Username]struct{})
+	workspace := make(map[utils.Username]struct{})
 	for _, ubmap := range localTable {
 		for user, _ := range ubmap {
 			workspace[user] = struct{}{}
@@ -204,16 +105,16 @@ func weekViewHandler(w http.ResponseWriter, r *http.Request) {
 	for _, game := range game_array {
 		userToBetMap := localTable[game]
 		fmt.Println("DEBUG LogAGame", game)
-		var bets []displayBets
+		var bets []utils.DisplayBets
 
 		for _, user := range users {
-			bet, present := userToBetMap[Username(user)]
+			bet, present := userToBetMap[utils.Username(user)]
 			if present {
 				fmt.Println("DEBUG LogBet Present", user)
 				bets = append(bets, bet)
 			} else {
 				fmt.Println("DEBUG LogBet NotPresent", user)
-				bets = append(bets, displayBets{"", "notstarted"})
+				bets = append(bets, utils.DisplayBets{"", "notstarted"})
 			}
 		}
 
