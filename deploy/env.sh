f903af00c7db3cdf20fd27111a698f4e0984e5ea
tjoshum <tjoshum@outlook.com>
2019-10-13 19:37:16 +0100

    Attempt to fix the rot

--- deploy/env.sh
+++ deploy/env.sh
@@ -7,7 +7,7 @@ fi
 
 if [ -z $DOCKER_ADDRESS ]; then
   # Assume we're running on linux, rather than a docker machine.
-  export DOCKER_ADDRESS=$(ifconfig docker0 | sed -n -e 's/.*inet addr:\([0-9]*\.*[0-9]*\.[0-9]\.[0-9]\).*/\1/p')
+  export DOCKER_ADDRESS=$(ifconfig docker0 | sed -n -e 's/.*inet \([0-9]*\.*[0-9]*\.[0-9]\.[0-9]\).*/\1/p')
 fi
 
 export REST_PORT="2901"
